160f37119bf693e8b6557e6d2afd5a03956289d7
viteras007 <viteras10@gmail.com>
2023-01-10 13:34:56 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request } from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
-
+import cors from 'cors';
 const app = express();
 
 mongoose.connect('mongodb://localhost/answerall-dev')
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
